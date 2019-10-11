84a8ce518f0b4f26fc52f7afea33db86a689fa6c
hchangjae <hcj@payw.org>
2019-10-11 16:59:29 +0900

    Devide developing activity by each developer

---
+++ Yofficial/app/src/main/java/com/example/yofficial/HyunWooActivity.java
@@ -0,0 +1,14 @@
+package com.example.yofficial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class HyunWooActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hyunwoo);
+    }
+}
--- Yofficial/app/src/main/java/com/example/yofficial/MainActivity.java
+++ Yofficial/app/src/main/java/com/example/yofficial/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.yofficial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void joonButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), JoonHongActivity.class);
+        startActivity(intent);
+    }
+
+    public void jaeButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), JaeWonActivity.class);
+        startActivity(intent);
+    }
+
+    public void hyunButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), HyunWooActivity.class);
+        startActivity(intent);
     }
 }
