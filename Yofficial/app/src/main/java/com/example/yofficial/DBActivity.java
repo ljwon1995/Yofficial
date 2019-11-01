d3ed5997b20a4c8c1c4b5f0208c222e70368bc70
JaeWon <ljwon1995@naver.com>
2019-11-01 17:44:27 +0900

    Practicing firebase

--- Yofficial/app/src/main/java/com/example/yofficial/DBActivity.java
+++ Yofficial/app/src/main/java/com/example/yofficial/DBActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 
@@ -14,10 +15,7 @@ public class DBActivity extends Activity {
     private static final String TAG = "DBActivity!";
 
 
-    TextView txtView;
-    String result;
-    SendDataToPHP sender;
-    String IPAdress = "http:/172.30.1.42:8080/GetValue.php";
+    EditText txtView;
 
 
 
@@ -25,20 +23,8 @@ public class DBActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db);
-        txtView = (TextView)findViewById(R.id.UpText);
+        txtView = findViewById(R.id.UpText);
 
-
-        sender = new SendDataToPHP(){
-            @Override
-            protected void onPostExecute(String str) {
-                super.onPostExecute(str);
-                Log.d(TAG, "Result = " + str);
-                result = str;
-                txtView.setText(result);
-            }
-        };
-        sender.makeQuery(SendDataToPHP.SELECT, "select * from person");
-        sender.execute(IPAdress);
     }
 
 
