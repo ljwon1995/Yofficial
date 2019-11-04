368e5b52669311a21c175b00fd9b1c42d0dd36d1
HyunWooParkKEIG <yey6687@daum.net>
2019-11-04 15:10:37 +0900

    implemented ingredient, seasoning, time tagging table insert/delete button reupload

--- Yofficial/app/src/main/java/com/example/yofficial/CreateRecipeActivity.java
+++ Yofficial/app/src/main/java/com/example/yofficial/CreateRecipeActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.io.InputStream;
 
@@ -29,12 +32,28 @@ public class CreateRecipeActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 0;
     private ImageView getImage;
-    private int timeIdCount = 0;
+    private int timeIdCount = 0; // 재료 추가 테이블 count
+    private int ssnIdCount = 0;  // 양념 추가 테이블 count
+
+    private int stageIdCount = 0; // 태깅 단계 추가 테이블 count
 
     //재료 테이블 받아오기
     TableRow tr[] = new TableRow[100];
     EditText ingName[] = new EditText[100];
     EditText ingFigures[] = new EditText[100];
+
+    //양념 테이블 받아오기
+    TableRow tr1[] = new TableRow[100];
+    EditText ssnName[] = new EditText[100];
+    EditText ssnFigures[] = new EditText[100];
+
+    //시간 태깅 단계 받아오기
+    TableRow tr2[][] = new TableRow[100][2];
+    EditText stageDescr[] = new EditText[100];
+    TextView startTime[] = new TextView[100];
+    TextView endTime[] = new TextView[100];
+    EditText stageEdit[][] = new EditText[100][6];
+
 
 
 
@@ -45,9 +64,15 @@ public class CreateRecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create);
 
         final TableLayout ingTable = findViewById(R.id.ingTable);
+        final TableLayout ssnTable = findViewById(R.id.ssnTable);
+        final TableLayout stageTable = findViewById(R.id.stageTable);
         TableRow.LayoutParams params = new TableRow.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams stage_params = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.weight = 1f;
+        stage_params.weight= 1f;
 
+
+        //재료 테이블 배열 초기화
         for (int i = 0; i < ingName.length; i++) {
             ingName[i] = new EditText(this);
             ingName[i].setLayoutParams(params);
@@ -66,9 +91,82 @@ public class CreateRecipeActivity extends AppCompatActivity {
             ingFigures[i].setTextSize(15);
         }
 
-
         for (int i = 0; i < tr.length; i++) {
             tr[i] = new TableRow(this);
+        }
+
+        //양념 테이블 배열 초기화
+        for (int i = 0; i < ssnName.length; i++) {
+            ssnName[i] = new EditText(this);
+            ssnName[i].setLayoutParams(params);
+            ssnName[i].setBackground(null);
+            ssnName[i].setHint("예시) 간장");
+            ssnName[i].setHintTextColor(Color.parseColor("#4075757E"));
+            ssnName[i].setTextSize(15);
+        }
+
+        for (int i = 0; i < ssnFigures.length; i++) {
+            ssnFigures[i] = new EditText(this);
+            ssnFigures[i].setLayoutParams(params);
+            ssnFigures[i].setBackground(null);
+            ssnFigures[i].setHint("예시) 2T");
+            ssnFigures[i].setHintTextColor(Color.parseColor("#4075757E"));
+            ssnFigures[i].setTextSize(15);
+        }
+
+
+        for (int i = 0; i < tr1.length; i++) {
+            tr1[i] = new TableRow(this);
+        }
+
+        // 시간 태깅 단계 설명 배열 초기화
+        for (int i = 0; i < stageDescr.length; i++) {
+            stageDescr[i] = new EditText(this);
+            stageDescr[i].setLayoutParams(params);
+            stageDescr[i].setPadding(0,50,0,0);
+            stageDescr[i].setBackground(null);
+            stageDescr[i].setHintTextColor(Color.parseColor("#4075757E"));
+            stageDescr[i].setTextSize(15);
+
+            int int_temp = i + 4;
+            String string_temp = Integer.toString(int_temp);
+            string_temp += "단계 영상설명 ex) 물을 끊입니다.";
+            stageDescr[i].setHint(string_temp);
+        }
+
+
+        for (int i = 0; i < startTime.length; i++) {
+            startTime[i] = new TextView(this);
+            startTime[i].setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            startTime[i].setText("시작 시간");
+            startTime[i].setTextColor(Color.parseColor("#000000"));
+        }
+
+        for (int i = 0; i < endTime.length; i++) {
+            endTime[i] = new TextView(this);
+            endTime[i].setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            endTime[i].setText("종료 시간");
+            endTime[i].setTextColor(Color.parseColor("#000000"));
+        }
+
+        for (int i = 0; i < stageEdit.length; i++) {
+            for (int j = 0; j < stageEdit[i].length; j++) {
+                stageEdit[i][j] = new EditText(this);
+                stageEdit[i][j].setLayoutParams(stage_params);
+                stageEdit[i][j].setBackgroundResource(R.drawable.edittext_teduri);
+                stageEdit[i][j].setTextSize(15);
+
+                InputFilter[] FilterArray = new InputFilter[1];
+                FilterArray[0] = new InputFilter.LengthFilter(2);
+                stageEdit[i][j].setFilters(FilterArray);
+                stageEdit[i][j].setInputType(InputType.TYPE_CLASS_NUMBER);
+            }
+        }
+
+        for (int i = 0; i < tr2.length; i++) {
+            for (int j = 0; j < tr2[i].length; j++) {
+                tr2[i][j] = new TableRow(this);
+            }
         }
 
         //재료 테이블 추가 삭제
@@ -91,6 +189,66 @@ public class CreateRecipeActivity extends AppCompatActivity {
                     tr[timeIdCount - 1].removeView(ingName[timeIdCount - 1]);
                     tr[timeIdCount - 1].removeView(ingFigures[timeIdCount - 1]);
                     timeIdCount--;
+                }
+            }
+        });
+
+        //양념 테이블 추가 삭제
+        Button insertSsnBtn = findViewById(R.id.insertSsn);
+        insertSsnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tr1[ssnIdCount].addView(ssnName[ssnIdCount]);
+                tr1[ssnIdCount].addView(ssnFigures[ssnIdCount]);
+                ssnTable.addView(tr1[ssnIdCount]);
+                ssnIdCount++;
+            }
+        });
+        Button removeSsnBtn = findViewById(R.id.removeSsn);
+        removeSsnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ssnIdCount > 0) {
+                    ssnTable.removeView(tr1[ssnIdCount - 1]);
+                    tr1[ssnIdCount - 1].removeView(ssnName[ssnIdCount - 1]);
+                    tr1[ssnIdCount - 1].removeView(ssnFigures[ssnIdCount - 1]);
+                    ssnIdCount--;
+                }
+            }
+        });
+
+        //시간 태깅 입력 추가
+
+        Button insertStage = (Button)findViewById(R.id.insertStage);
+        insertStage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stageTable.addView(stageDescr[stageIdCount]);
+                tr2[stageIdCount][0].addView(startTime[stageIdCount]);
+                tr2[stageIdCount][0].addView(stageEdit[stageIdCount][0]);
+                tr2[stageIdCount][0].addView(stageEdit[stageIdCount][1]);
+                tr2[stageIdCount][0].addView(stageEdit[stageIdCount][2]);
+                tr2[stageIdCount][1].addView(endTime[stageIdCount]);
+                tr2[stageIdCount][1].addView(stageEdit[stageIdCount][3]);
+                tr2[stageIdCount][1].addView(stageEdit[stageIdCount][4]);
+                tr2[stageIdCount][1].addView(stageEdit[stageIdCount][5]);
+                stageTable.addView(tr2[stageIdCount][0]);
+                stageTable.addView(tr2[stageIdCount][1]);
+                stageIdCount++;
+            }
+        });
+
+        Button removeStage = (Button)findViewById(R.id.removeStage);
+        removeStage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (stageIdCount > 0) {
+                    stageTable.removeView(tr2[stageIdCount - 1][1]);
+                    stageTable.removeView(tr2[stageIdCount - 1][0]);
+                    tr2[stageIdCount - 1][1].removeView(endTime[stageIdCount - 1]);
+                    tr2[stageIdCount - 1][0].removeView(startTime[stageIdCount - 1]);
+                    stageTable.removeView(stageDescr[stageIdCount - 1]);
+                    stageIdCount--;
                 }
             }
         });
@@ -127,16 +285,6 @@ public class CreateRecipeActivity extends AppCompatActivity {
         featureSpinner.setAdapter(featureAdapter);
 
 
-        //시간 태깅 스피너 선언부
-        Spinner  s_hour1= (Spinner)findViewById(R.id.s_hour1);
-        ArrayAdapter s_hour1Adapter = ArrayAdapter.createFromResource(this, R.array.data_hour, android.R.layout.simple_spinner_item);
-        s_hour1.setAdapter(s_hour1Adapter);
-        Spinner  s_minute1= (Spinner)findViewById(R.id.s_minute1);
-        ArrayAdapter s_minute1Adapter = ArrayAdapter.createFromResource(this, R.array.data_minute, android.R.layout.simple_spinner_item);
-        s_minute1.setAdapter(s_minute1Adapter);
-        Spinner  s_second1= (Spinner)findViewById(R.id.s_second1);
-        ArrayAdapter s_second1Adapter = ArrayAdapter.createFromResource(this, R.array.data_second, android.R.layout.simple_spinner_item);
-        s_second1.setAdapter(s_second1Adapter);
 
         // 인분, 난이도, 소요시간 다중 선택 부
         final TextView servingSelB = (TextView) findViewById(R.id.servingSel);
@@ -212,21 +360,7 @@ public class CreateRecipeActivity extends AppCompatActivity {
             }
         });
 
-        //시간 태깅 입력 추가
-        TableLayout timeTable = (TableLayout)findViewById(R.id.timeTable);
-        timeTable.setStretchAllColumns(true);
-        TableRow tr1 = new TableRow(this);
-        tr1.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
 
-
-
-        Button insert = (Button)findViewById(R.id.insertTimeButton);
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
 
