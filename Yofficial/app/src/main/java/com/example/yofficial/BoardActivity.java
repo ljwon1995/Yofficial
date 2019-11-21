1f17254c2bc7b0a71e80beac56b20dcb448e3109
JaeWon <ljwon1995@naver.com>
2019-11-21 12:27:01 +0900

    Finish interlocking board activity with db

--- Yofficial/app/src/main/java/com/example/yofficial/BoardActivity.java
+++ Yofficial/app/src/main/java/com/example/yofficial/BoardActivity.java
@@ -6,7 +6,14 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BoardActivity extends AppCompatActivity {
 
@@ -15,26 +22,40 @@ public class BoardActivity extends AppCompatActivity {
     TextView title;
     TextView info;
     TextView body;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_activity);
 
-        item = new BoardItem("1", "\n탕수육을 왜먹냐? 그 돈으로 국밥 3그릇을 먹지", "박현우", "2018/01/01", "아무튼 국밥임");
 
         Intent intent = getIntent();
         id =intent.getExtras().getString("id");
 
 
-        if(id.equals(item.board_id)){
-            title = (TextView)findViewById(R.id.board_title);
-            info = (TextView)findViewById(R.id.board_info);
-            body = (TextView)findViewById(R.id.board_data);
 
-            title.setText(item.board_title);
-            info.setText(item.board_uploader + " | " + item.board_date);
-            body.setText(item.board_data);
-        }
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        myRef.child("boards").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                item = dataSnapshot.getValue(BoardItem.class);
+                title = (TextView)findViewById(R.id.board_title);
+                info = (TextView)findViewById(R.id.board_info);
+                body = (TextView)findViewById(R.id.board_data);
+                title.setText(item.board_title);
+                info.setText(item.board_uploader + " | " + item.board_date);
+                body.setText(item.board_data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
 
     }
